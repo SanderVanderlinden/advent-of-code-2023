ea318b92e4fd3cd498c2a8c3c1be70d2d221410b
Vanderlinden, Sander <svanderlinden@mazdaeur.com>
2023-12-01 17:49:08 +0100

    Refactor FileReaderUtil.java, add javadoc and improve error handling

--- app/src/main/java/com/sandervanderlinden/adventofcode2023/utils/FileReaderUtil.java
+++ app/src/main/java/com/sandervanderlinden/adventofcode2023/utils/FileReaderUtil.java
@@ -7,17 +7,23 @@ import java.util.function.ToIntFunction;
 
 public class FileReaderUtil {
 
+    /**
+     * Processes each line of the file specified by the filePath using the provided lineProcessor function.
+     *
+     * @param filePath      The path to the file to be processed.
+     * @param lineProcessor A function to process each line of the file.
+     * @return The sum of the processed values of each line.
+     * @throws RuntimeException if there is an issue with file processing.
+     */
     public static int processFile(String filePath, ToIntFunction<String> lineProcessor) {
         InputStream inputStream = FileReaderUtil.class.getResourceAsStream(filePath);
         if (inputStream == null) {
-            System.err.println("Input file not found");
-            return 0;
+            throw new RuntimeException("Input file not found: " + filePath);
         }
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
             return reader.lines().mapToInt(lineProcessor).sum();
         } catch (Exception e) {
-            e.printStackTrace();
-            return 0;
+            throw new RuntimeException("Error processing file: " + filePath, e);
         }
     }
 }
