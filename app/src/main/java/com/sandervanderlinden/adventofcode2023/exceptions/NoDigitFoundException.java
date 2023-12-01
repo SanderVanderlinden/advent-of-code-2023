2a6316a81c685ba7d49160f57aeeaca82018c767
Vanderlinden, Sander <svanderlinden@mazdaeur.com>
2023-12-01 14:43:56 +0100

    Change NoDigitFoundException to extend RuntimeException

--- app/src/main/java/com/sandervanderlinden/adventofcode2023/exceptions/NoDigitFoundException.java
+++ app/src/main/java/com/sandervanderlinden/adventofcode2023/exceptions/NoDigitFoundException.java
@@ -1,6 +1,6 @@
 package com.sandervanderlinden.adventofcode2023.exceptions;
 
-public class NoDigitFoundException extends Exception {
+public class NoDigitFoundException extends RuntimeException  {
 
     public NoDigitFoundException(String message) {
         super(message);
