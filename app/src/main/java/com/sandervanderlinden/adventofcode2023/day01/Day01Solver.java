fe049794a7404c107eaf6a0a3038d2eb0deeb91a
Vanderlinden, Sander <svanderlinden@mazdaeur.com>
2023-12-01 12:29:53 +0100

    Create app file and set up reading capabilities to access input file

---
+++ app/src/main/java/com/sandervanderlinden/adventofcode2023/day01/Day01Solver.java
@@ -0,0 +1,17 @@
+package com.sandervanderlinden.adventofcode2023.day01;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class Day01Solver {
+    public static void main(String[] args) {
+        InputStream inputStream = Day01Solver.class.getResourceAsStream("/day01/day01_input.txt");
+        if (inputStream != null) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            reader.lines().forEach(System.out::println);
+        } else {
+            System.err.println("Input file not found");
+        }
+    }
+}
