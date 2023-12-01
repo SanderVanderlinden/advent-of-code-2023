eda74361f5994dda85c16aef294831f31ffba4d8
Vanderlinden, Sander <svanderlinden@mazdaeur.com>
2023-12-01 14:11:44 +0100

    Refactor test methods

--- app/src/test/java/com/sandervanderlinden/adventofcode2023/day01/Day01SolverTest.java
+++ app/src/test/java/com/sandervanderlinden/adventofcode2023/day01/Day01SolverTest.java
@@ -5,25 +5,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day01SolverTest {
 
+    private final Day01Solver solver = new Day01Solver();
+
+
     @Test
-    void extractAndSumNumbers_ShouldReturnSumOfFirstAndLastDigit() {
-        Day01Solver solver = new Day01Solver();
+    void extractAndSumNumbers_whenTypicalLine_shouldReturnSumOfFirstAndLastDigit() {
         String line = "example1line2";
         int expected = 12;
         assertEquals(expected, solver.extractAndSumNumbers(line));
     }
 
     @Test
-    void extractFirstNumber() {
-        Day01Solver solver = new Day01Solver();
+    void extractFirstNumber_whenTypicalLine_shouldReturnFirstDigit() {
         String line = "example1line2";
         int expected = 1;
         assertEquals(expected, solver.extractFirstNumber(line));
     }
 
     @Test
-    void extractLastNumber() {
-        Day01Solver solver = new Day01Solver();
+    void extractLastNumber_whenTypicalLine_shouldReturnLastDigit() {
         String line = "example1line2";
         int expected = 2;
         assertEquals(expected, solver.extractLastNumber(line));
