13afb0d84bee533f06aad7ee2eeb9c34e1fe7b1c
Vanderlinden, Sander <svanderlinden@mazdaeur.com>
2023-12-01 14:31:46 +0100

    Create NoDigitFoundException

---
+++ app/src/main/java/com/sandervanderlinden/adventofcode2023/exceptions/NoDigitFoundException.java
@@ -0,0 +1,10 @@
+package com.sandervanderlinden.adventofcode2023.exceptions;
+
+public class NoDigitFoundException extends Exception {
+
+    public NoDigitFoundException(String message) {
+        super(message);
+    }
+
+    // You can also add additional constructors or methods if needed
+}
