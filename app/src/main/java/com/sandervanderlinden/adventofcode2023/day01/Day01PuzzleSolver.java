3b1f3cbcef267aff11bd02308d85ec445cb577c1
Vanderlinden, Sander <svanderlinden@mazdaeur.com>
2023-12-02 23:17:06 +0100

    Refactor day 01 code to make this more testable in a general test folder

--- app/src/main/java/com/sandervanderlinden/adventofcode2023/day01/Day01PuzzleSolver.java
+++ app/src/main/java/com/sandervanderlinden/adventofcode2023/day01/Day01PuzzleSolver.java
@@ -1,8 +1,22 @@
 package com.sandervanderlinden.adventofcode2023.day01;
 
 import com.sandervanderlinden.adventofcode2023.exceptions.NoDigitFoundException;
+import com.sandervanderlinden.adventofcode2023.utils.FileReaderUtil;
 
-interface Day01PuzzleSolver {
+public interface Day01PuzzleSolver {
+
+
+    /**
+     * Solves the puzzle by processing the file specified by filePath.
+     * It applies the extractAndCombineDigits method to each line of the file
+     * to calculate a combined sum of the processed digits.
+     *
+     * @param filePath The relative path of the file to be processed.
+     * @return The sum of the processed digits as an integer.
+     */
+    default int solve(String filePath) {
+        return FileReaderUtil.processFile(filePath, this::extractAndCombineDigits);
+    }
 
     /**
      * Extracts the first and last digit from a line and combines them into a two-digit number.
