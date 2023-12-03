70885a2e5a9036d4434b022532e686ca087a409c
Vanderlinden, Sander <svanderlinden@mazdaeur.com>
2023-12-03 02:51:27 +0100

    Update readme and create interface for second puzzle of the day.

--- app/src/main/java/com/sandervanderlinden/adventofcode2023/day02/Day02Puzzle01Solver.java
+++ app/src/main/java/com/sandervanderlinden/adventofcode2023/day02/Day02Puzzle01Solver.java
@@ -1,72 +1,20 @@
 package com.sandervanderlinden.adventofcode2023.day02;
 
-import com.sandervanderlinden.adventofcode2023.day02.game.Color;
-import com.sandervanderlinden.adventofcode2023.day02.game.CubeReveal;
-import com.sandervanderlinden.adventofcode2023.day02.game.Game;
-import com.sandervanderlinden.adventofcode2023.utils.FileReaderUtil;
+/**
+ * Solver for Day 2 Puzzle 1 of Advent of Code 2023.
+ * This solver processes game data to determine which games are possible
+ * given specific constraints on the number of cubes.
+ */
+public class Day02Puzzle01Solver implements Day02PuzzleSolver {
 
-import java.util.EnumMap;
-import java.util.HashSet;
-import java.util.Set;
-
-public class Day02Puzzle01Solver {
-    private static final EnumMap<Color, Integer> cubesInBag;
-
-    static {
-        cubesInBag = new EnumMap<>(Color.class);
-        cubesInBag.put(Color.RED, 12);   // Example values
-        cubesInBag.put(Color.GREEN, 13);
-        cubesInBag.put(Color.BLUE, 14);
-    }
-
+    /**
+     * Main method to execute the solver.
+     * Processes the input file and calculates the total sum of the IDs of possible games.
+     */
     public static void main(String[] args) {
         Day02Puzzle01Solver solver = new Day02Puzzle01Solver();
         int sum = solver.solve();
         System.out.println("Total sum: " + sum);
-    }
-
-    public int solve() {
-        return FileReaderUtil.processFile("/day02/day02_input.txt", this::getIdValue);
-    }
-
-
-    /**
-     * Returns the id of the game, if the game is possible and 0 otherwise.
-     *
-     * @param line The string line to extract digits from.
-     * @return game id or 0.
-     */
-    public int getIdValue(String line) {
-        Game game = createGameFromLine(line);
-        if (game.isPossible()) {
-            return game.getId();
-        }
-        return 0;
-    }
-
-    public Game createGameFromLine(String line) {
-        return new Game(extractGameId(line), extractCubeReveals(line), cubesInBag);
-    }
-
-    private Set<CubeReveal> extractCubeReveals(String line) {
-        String cubeRevealsString = line.substring(line.indexOf(":") + 1).trim();
-
-        String[] separators = cubeRevealsString.split("[,;]");
-
-        Set<CubeReveal> cubeReveals = new HashSet<>();
-
-        for (String separator : separators) {
-            String[] cubeRevealData = separator.trim().split(" ");
-            Color color = Color.fromString(cubeRevealData[1]);
-            int amount = Integer.parseInt(cubeRevealData[0]);
-            cubeReveals.add(new CubeReveal(color, amount));
-        }
-        return cubeReveals;
-    }
-
-    public int extractGameId(String line) {
-        String gameId = line.substring(5, line.indexOf(":"));
-        return Integer.parseInt(gameId);
     }
 
 
--- app/src/main/resources/day02/README.md
+++ app/src/main/resources/day02/README.md
@@ -32,6 +32,29 @@ and 14 blue cubes. Sum the IDs of the games that meet this criteria.
 
 Identify and calculate the sum of the IDs of the possible games given the constraints on the number of cubes.
 
+## --- Part Two ---
+
+### Problem Description
+
+The game continues with a new twist. Now, the Elf says you must determine the minimum number of cubes of each color that
+could have been in the bag for each game to be possible.
+
+### Example
+
+- `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green`
+- `Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue`
+- `Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red`
+- `Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red`
+- `Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`
+
+**Explanation:**
+
+The power of a set of cubes is the product of the numbers of red, green, and blue cubes. For example, the power of the
+minimum set in Game 1 is 48 (4 red * 2 green * 6 blue).
+
+### Task
+
+Calculate the sum of the power of these minimum sets for each game.
 
 ---
 
