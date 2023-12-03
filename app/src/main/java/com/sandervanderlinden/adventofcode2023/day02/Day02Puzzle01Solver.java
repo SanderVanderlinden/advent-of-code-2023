fc5e9f866883dc0e426a5df44a6125eb68223cae
Vanderlinden, Sander <svanderlinden@mazdaeur.com>
2023-12-03 01:42:26 +0100

    Add createGameFromLine logic in both solver and game classes

--- app/src/main/java/com/sandervanderlinden/adventofcode2023/day02/Day02Puzzle01Solver.java
+++ app/src/main/java/com/sandervanderlinden/adventofcode2023/day02/Day02Puzzle01Solver.java
@@ -1,12 +1,12 @@
 package com.sandervanderlinden.adventofcode2023.day02;
 
+import com.sandervanderlinden.adventofcode2023.day02.game.Color;
+import com.sandervanderlinden.adventofcode2023.day02.game.CubeReveal;
 import com.sandervanderlinden.adventofcode2023.day02.game.Game;
-import com.sandervanderlinden.adventofcode2023.exceptions.NoDigitFoundException;
 import com.sandervanderlinden.adventofcode2023.utils.FileReaderUtil;
 
 import java.util.HashSet;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Set;
 
 public class Day02Puzzle01Solver {
     public static void main(String[] args) {
@@ -30,18 +30,29 @@ public class Day02Puzzle01Solver {
     }
 
     public Game createGameFromLine(String line) {
-        //TODO implement
-        return new Game(extractGameId(line), new HashSet<>(), 0, 0, 0);
+        return new Game(extractGameId(line), extractCubeReveals(line));
+    }
+
+    private Set<CubeReveal> extractCubeReveals(String line) {
+        String cubeRevealsString = line.substring(line.indexOf(":") + 1).trim();
+
+        String[] separators = cubeRevealsString.split("[,;]");
+
+        Set<CubeReveal> cubeReveals = new HashSet<>();
+
+        for (String separator : separators) {
+            String[] cubeRevealData = separator.trim().split(" ");
+            Color color = Color.fromString(cubeRevealData[1]);
+            int amount = Integer.parseInt(cubeRevealData[0]);
+            cubeReveals.add(new CubeReveal(color, amount));
+        }
+        return cubeReveals;
     }
 
     public int extractGameId(String line) {
-        Pattern pattern = Pattern.compile("\\d+");
-        Matcher matcher = pattern.matcher(line);
-
-        if (matcher.find()) {
-            return Integer.parseInt(matcher.group());
-        }
-        throw new NoDigitFoundException("No Id found in the line: " + line);
+        String gameId = line.substring(5, line.indexOf(":"));
+        return Integer.parseInt(gameId);
     }
+
 
 }
--- app/src/main/java/com/sandervanderlinden/adventofcode2023/day02/game/Game.java
+++ app/src/main/java/com/sandervanderlinden/adventofcode2023/day02/game/Game.java
@@ -1,30 +1,50 @@
 package com.sandervanderlinden.adventofcode2023.day02.game;
 
+import java.util.EnumMap;
+import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Stream;
 
 public class Game {
     private final int id;
     private final Set<CubeReveal> cubeReveals;
-    private int greenCubesMaximumShown;
-    private int redCubesMaximumShown;
-    private int blueCubesMaximumShown;
+    private final EnumMap<Color, Integer> revealedMaximums;
+
 
     public Game(int id, Set<CubeReveal> cubeReveals) {
         this.id = id;
         this.cubeReveals = cubeReveals;
-        calculateMaximums();
+        this.revealedMaximums = new EnumMap<>(Color.class);
+        Stream.of(Color.values())
+                .forEach(this::setMaximum);
     }
 
-    private void calculateMaximums() {
-        //TODO implement
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Game game)) return false;
+        return getId() == game.getId() && Objects.equals(cubeReveals, game.cubeReveals);
+    }
+
+    private void setMaximum(Color color) {
+        revealedMaximums.put(color, findMaximum(color));
+    }
+
+    private int findMaximum(Color color) {
+        return cubeReveals.stream()
+                .filter(c -> c.color() == color)
+                .mapToInt(CubeReveal::amount)
+                .max()
+                .orElse(0);
     }
 
     public boolean isPossible() {
         //TODO implement
         return true;
-    }
-
-    public int getId() {
-        return id;
     }
 }
