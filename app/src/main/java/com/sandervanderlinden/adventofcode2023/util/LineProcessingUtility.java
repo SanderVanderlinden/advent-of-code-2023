bc5b5e714f0d8976b9812d11aa0ef40f1bcb0d49
Sander Vanderlinden <sander-vanderlinden@hotmail.com>
2023-12-10 16:34:26 +0100

    Refactor LineProcessingUtility

--- app/src/main/java/com/sandervanderlinden/adventofcode2023/util/LineProcessingUtility.java
+++ app/src/main/java/com/sandervanderlinden/adventofcode2023/util/LineProcessingUtility.java
@@ -5,21 +5,27 @@ import com.sandervanderlinden.adventofcode2023.exceptions.NoDigitFoundException;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/**
+ * Utility class for processing lines of text with various string operations.
+ */
 public class LineProcessingUtility {
 
     private static final Pattern DIGIT_PATTERN = Pattern.compile("\\d");
+    private static final Pattern NON_DIGIT_PATTERN = Pattern.compile("\\D+");
+    private static final Pattern SPACE_PATTERN = Pattern.compile("\\s+");
 
     LineProcessingUtility() {
     }
 
     /**
-     * Extracts the first digit found in a line.
+     * Extracts the first digit found in a given string.
      *
-     * @param line The string line destination extract the first digit from.
-     * @return The first digit found in the line.
-     * @throws NoDigitFoundException if no digit is found in the line.
+     * @param line the string to search for digits
+     * @return the first digit found as an integer
+     * @throws NoDigitFoundException if no digit is found in the string
      */
     public static int extractFirstDigit(String line) throws NoDigitFoundException {
         Matcher matcher = DIGIT_PATTERN.matcher(line);
@@ -31,52 +37,72 @@ public class LineProcessingUtility {
     }
 
     /**
-     * Extracts the last digit found in a line.
+     * Extracts the last digit found in a given string.
      *
-     * @param line The string line destination extract the last digit from.
-     * @return The last digit found in the line.
-     * @throws NoDigitFoundException if no digit is found in the line.
+     * @param line the string to search for digits
+     * @return the last digit found as an integer
+     * @throws NoDigitFoundException if no digit is found in the string
      */
     public static int extractLastDigit(String line) throws NoDigitFoundException {
-        String reversedLine = new StringBuilder(line).reverse().toString();
-        return extractFirstDigit(reversedLine);
+        Matcher matcher = DIGIT_PATTERN.matcher(line);
+        int lastDigit = -1;
+
+        while (matcher.find()) {
+            lastDigit = Integer.parseInt(matcher.group());
+        }
+
+        if (lastDigit != -1) {
+            return lastDigit;
+        }
+        throw new NoDigitFoundException("No digits found in the line: " + line);
     }
 
+    /**
+     * Extracts all numbers from a string and returns them as a stream of longs.
+     *
+     * @param text the string to extract numbers from
+     * @return a stream of longs representing the numbers found in the string
+     */
     public static Stream<Long> extractNumbersAsLongStream(String text) {
-        return Pattern.compile("\\D+")
-                .splitAsStream(text)
+        return NON_DIGIT_PATTERN.splitAsStream(text)
                 .filter(str -> !str.isEmpty())
                 .map(Long::parseLong);
     }
 
+    /**
+     * Extracts all numbers from a string and concatenates them into a single long.
+     *
+     * @param text the string to extract numbers from
+     * @return a long representing all concatenated numbers found in the string
+     */
     public static long extractNumbersAsOneNumber(String text) {
-        var numbersStringList =  Pattern.compile("\\D+")
-                .splitAsStream(text)
+        return Long.parseLong(NON_DIGIT_PATTERN.splitAsStream(text)
                 .filter(str -> !str.isEmpty())
-                .toList();
-
-        StringBuilder outputString = new StringBuilder();
-
-        for (String numberString : numbersStringList){
-            outputString.append(numberString);
-        }
-        return Long.parseLong(outputString.toString());
+                .collect(Collectors.joining()));
     }
 
+    /**
+     * Extracts all numbers from a string and returns them as a list of longs.
+     *
+     * @param text the string to extract numbers from
+     * @return a list of longs representing the numbers found in the string
+     */
     public static List<Long> extractNumbersAsList(String text) {
-        return Pattern.compile("\\D+")
-                .splitAsStream(text)
+        return NON_DIGIT_PATTERN.splitAsStream(text)
                 .filter(str -> !str.isEmpty())
                 .map(Long::parseLong)
                 .toList();
     }
 
+    /**
+     * Converts a space-separated string into a list of strings.
+     *
+     * @param text the string to be split into a list
+     * @return a list of strings, split by spaces
+     */
     public static List<String> spaceSeparatedStringToList(String text) {
-        return Pattern.compile("\\s+")
-                .splitAsStream(text)
+        return SPACE_PATTERN.splitAsStream(text)
                 .filter(str -> !str.isEmpty())
                 .toList();
     }
-
-
 }
